a2d18bb4833e0dd5f8402a64d57c10b16bae91b6
dmikitiuk <damianmikitiuk@gmail.com>
2018-08-19 18:05:14 +0200

    Http Interceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'http-interceptor-example';
+
+  constructor(private http: HttpClient,
+    private toastr: ToastrService) { }
+
+  onSendCorrect() {
+    // Request to get the photo with the '2' ID
+    this.http.get('https://jsonplaceholder.typicode.com/photos/2').subscribe(
+      response => {
+        if (response) {
+          this.toastr.success('😃', 'Request Correct');
+        }
+      }
+    );
+  }
+
+  onSend404() {
+    // Request to get the photo with unknow photo ID
+    this.http.get('https://jsonplaceholder.typicode.com/photos/dd').subscribe();
+  }
+
+  onSend500() {
+    // Request to put not allowed data to the photo with '1' ID
+    this.http.put('https://jsonplaceholder.typicode.com/photos/1', 2).subscribe();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { ErrorHandlerInterceptor } from './error-handler-interceptor/error-handler-interceptor';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-center'
+    })
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: ErrorHandlerInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
